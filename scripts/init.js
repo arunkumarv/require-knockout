34ba5bb479e7b9f11d54f5ff0c92fa4a4fa2356d
Arunkumar Vasudevan <arunkumar.v@oracle.com>
2022-07-07 13:29:48 +0530

    a simple popup on hover

--- scripts/init.js
+++ scripts/init.js
@@ -1,4 +1,4 @@
-require(['libs/knockout-3.5.1', 'sectionOneViewModel', 'sectionTwoViewModel', 'sectionThreeViewModel', 'customBindingHandlers/somebindingHandlers'], function (ko, sectionOneViewModel, sectionTwoViewModel, sectionThreeViewModel) {
+require(['libs/knockout-3.5.1', 'sectionOneViewModel', 'sectionTwoViewModel', 'sectionThreeViewModel', 'nativePopViewModel', 'customBindingHandlers/somebindingHandlers'], function (ko, sectionOneViewModel, sectionTwoViewModel, nativePopViewModel, sectionThreeViewModel) {
 
     function httpGetAsync(theUrl, callback) {
         var xmlHttp = new XMLHttpRequest();
@@ -10,15 +10,9 @@ require(['libs/knockout-3.5.1', 'sectionOneViewModel', 'sectionTwoViewModel', 's
         xmlHttp.send(null);
     }
 
-    httpGetAsync("/scripts/templates.html", function (response) {
-        let div = document.createElement('div');
-        div.innerHTML = response;
-        div.style.display = 'none';
-        document.body.appendChild(div);
-
-        bindAll();
-    })
     function bindAll() {
+
+        ko.applyBindings(new nativePopViewModel(), document.getElementById("native-pop"));
 
         ko.applyBindings(new sectionOneViewModel(), document.getElementById("section-one"));
 
@@ -26,4 +20,43 @@ require(['libs/knockout-3.5.1', 'sectionOneViewModel', 'sectionTwoViewModel', 's
 
         ko.applyBindings(new sectionThreeViewModel(), document.getElementById("section-three"));
     }
+
+    httpGetAsync("/scripts/templates.html", function (response) {
+        let div = document.createElement('div');
+        div.innerHTML = response;
+        div.style.display = 'none';
+        document.body.appendChild(div);
+
+        bindAll();
+    });
+
+    function attachPopup(subject, popup) {
+        let boundingClientRect = subject.getBoundingClientRect();
+        popup.style.top = boundingClientRect.top;
+        popup.style.left = boundingClientRect.right;
+    }
+
+    let subject = document.getElementById('subject');
+    let outer = document.getElementById('outer');
+    let popup = document.getElementById('popup');
+
+    attachPopup(subject, popup);
+
+    let doIt;
+    const resizeObserver = new ResizeObserver((entries) => {
+        clearTimeout(doIt);
+        doIt = setTimeout( function(){
+            attachPopup(subject, popup)
+        }, 100);
+    });
+
+    outer.addEventListener('mouseenter', function () {
+        popup.style.display = "block";
+    })
+
+    outer.addEventListener('mouseleave', function () {
+        popup.style.display = "none";
+    })
+    console.log(subject)
+    resizeObserver.observe(subject);
 });
---
+++ scripts/nativePopViewModel.js
@@ -0,0 +1,11 @@
+define([], function (){
+
+    class nativePopViewModel {
+        
+        constructor() {
+
+        }
+    }
+
+    return nativePopViewModel;
+})
