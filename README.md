458c670d606a6914fc87dd66794b5d58cb4b71df
Arunkumar Vasudevan <arunkumar.v@oracle.com>
2022-07-07 11:17:30 +0530

    Load template from another file

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Notes and samples on requirejs with knockoutjs
 - [x] view ViewModel connection
 - [x] added template binding
 - [x] binding handler
+- [x] Load template from another file
 - [ ] Add jquery
 - [ ] move init and appviewmodel to an src folder
 - [ ] standardise paths
--- scripts/init.js
+++ scripts/init.js
@@ -1,8 +1,29 @@
-require(['libs/knockout-3.5.1', 'sectionOneViewModel', 'sectionTwoViewModel', 'sectionThreeViewModel', 'customBindingHandlers/somebindingHandlers'], function(ko, sectionOneViewModel, sectionTwoViewModel, sectionThreeViewModel) {
+require(['libs/knockout-3.5.1', 'sectionOneViewModel', 'sectionTwoViewModel', 'sectionThreeViewModel', 'customBindingHandlers/somebindingHandlers'], function (ko, sectionOneViewModel, sectionTwoViewModel, sectionThreeViewModel) {
 
-    ko.applyBindings(new sectionOneViewModel(), document.getElementById("section-one"));
+    function httpGetAsync(theUrl, callback) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback(xmlHttp.responseText);
+        }
+        xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+        xmlHttp.send(null);
+    }
 
-    ko.applyBindings(new sectionTwoViewModel(), document.getElementById("section-two"));
+    httpGetAsync("/scripts/templates.html", function (response) {
+        let div = document.createElement('div');
+        div.innerHTML = response;
+        div.style.display = 'none';
+        document.body.appendChild(div);
 
-    ko.applyBindings(new sectionThreeViewModel(), document.getElementById("section-three"));
+        bindAll();
+    })
+    function bindAll() {
+
+        ko.applyBindings(new sectionOneViewModel(), document.getElementById("section-one"));
+
+        ko.applyBindings(new sectionTwoViewModel(), document.getElementById("section-two"));
+
+        ko.applyBindings(new sectionThreeViewModel(), document.getElementById("section-three"));
+    }
 });
